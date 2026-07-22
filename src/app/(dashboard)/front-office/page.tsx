704408e0d717216618ab398c4cb688e45d194fe1
samani21 <eksloba21@gmail.com>
2026-07-22 11:20:34 +0800

    Tambahkan tombol aktifkan dan matikan is_active user biar tidak bisa login

---
+++ src/app/(dashboard)/front-office/page.tsx
@@ -0,0 +1,12 @@
+import MainLayout from '@/Components/Layout/MainLayout'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <MainLayout>page</MainLayout>
+    )
+}
+
+export default page
--- src/app/superadmin/master/front-office/Components/Type.ts
+++ src/app/superadmin/master/front-office/Components/Type.ts
@@ -2,4 +2,5 @@ export interface FrontOfficeType {
     id: number;
     name: string;
     email: string;
+    is_active: boolean;
 }
